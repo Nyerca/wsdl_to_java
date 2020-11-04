17ad2734365730352856d83b891a754b39c2ed0c
EnricoFacciani <efacciani@imolainformatica.it>
2020-11-04 12:25:52 +0100

    Add elements to xsd

--- src/main/java/com/mycompany/app/EmployeeServiceTopDownImpl.java
+++ src/main/java/com/mycompany/app/EmployeeServiceTopDownImpl.java
@@ -16,6 +16,10 @@ public class EmployeeServiceTopDownImpl
     public int countEmployees(CountEmployees parameters) {
 
         System.out.println("Num: " + parameters.getEmployee().size());
+        for(int i=0; i < parameters.getEmployee().size(); i++) {
+            CountEmployees.Employee emp = parameters.getEmployee().get(i);
+            System.out.println("Name: " + emp.getName() + " pay: " + emp.getPay() + " contribution: " + emp.getContribution());
+        }
         return parameters.getEmployee().size();
     }
 }
